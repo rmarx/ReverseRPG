a44a15c62b4f3030a3283fe94a6e893874b5fdf3
geoffry_nagy <gnagy@talentina.com>
2014-01-25 11:50:06 +0100

    Added teddybear and teddybear holes prefabs, added vicinityscript, added instantiation script

---
+++ ReverseRPG/Assets/StartingScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartingScript : MonoBehaviour {
+	public GameObject teddyBear;
+	public GameObject teddyBearHole;
+	public GameObject teddyBearHolesContainer;
+
+	public int numberOfTeddyBears = 5;
+	public int numberOfTeddyBearHoles = 5;
+
+	public float startingXOfTeddyBear = -11.0f;
+	public float startingYOfTeddyBear = 0.7172546f;
+	public float startingZOfTeddyBear = -2.0f;
+	public float startingXOfTeddyBearHole = -32.0f;
+	public float startingYOfTeddyBearHole = 3.778082f;
+	public float startingZOfTeddyBearHole = -2.0f;
+
+	public float xMultiplierOfTeddyBear = 1.0f;
+	public float yMultiplierOfTeddyBear = 0.0f;
+	public float zMultiplierOfTeddyBear = 5.0f;
+	public float xMultiplierOfTeddyBearHole = 1.0f;
+	public float yMultiplierOfTeddyBearHole = 0.0f;
+	public float zMultiplierOfTeddyBearHole = 10.0f;
+
+	// Use this for initialization
+	void Awake () {
+		if (teddyBearHolesContainer == null) 
+		{
+			teddyBearHolesContainer = GameObject.Find ("Holes");
+		}
+
+		//instantiate 5 Teddybears
+		for (int i = 0; i < numberOfTeddyBears; i++) 
+		{
+			GameObject.Instantiate (teddyBear, new Vector3(startingXOfTeddyBear + (xMultiplierOfTeddyBear * i), 
+			                                               startingYOfTeddyBear + (yMultiplierOfTeddyBear * i), 
+			                                               startingZOfTeddyBear + (zMultiplierOfTeddyBear * i)), Quaternion.identity);	 	
+		}
+		GameObject.Destroy (teddyBear);
+		for (int i = 0; i < numberOfTeddyBearHoles; i++) 
+		{
+			GameObject temp = (GameObject) GameObject.Instantiate(teddyBearHole, new Vector3(startingXOfTeddyBearHole + (xMultiplierOfTeddyBearHole * i), 
+			                                                                                 startingYOfTeddyBearHole + (yMultiplierOfTeddyBearHole * i), 
+			                                                                                 startingZOfTeddyBearHole + (zMultiplierOfTeddyBearHole * i)), Quaternion.identity);
+			temp.transform.parent = teddyBearHolesContainer.transform;
+		}
+		GameObject.Destroy (teddyBearHole);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ReverseRPG/Assets/VicinityBehaviour.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VicinityBehaviour : MonoBehaviour {
+
+	public float moveSpeed = 50.0f;
+	public GameObject mainCharacter = null;
+	public GameObject teddyBearHolesParent;
+	public Transform[] teddyBearHolesChildren;
+	public Vector3 directionToMainCharacter;
+	public Vector3 directionToMove;
+	public bool mainCharacterIsClose = false;
+	public bool startRunningToHole = false;
+	public Transform closestHole = null;
+
+
+	public void SetupLocal()
+	{
+	}
+	
+	public void SetupGlobal()
+	{
+		// lookup references to objects / scripts outside of this script
+		//GameObject.FindObjectsOfType<VicinityBehaviour>
+		
+		// assign variables that have to do with this class only
+		if (mainCharacter == null) 
+		{
+			mainCharacter = GameObject.Find ("Character");
+		}
+		if (teddyBearHolesParent == null) 
+		{
+			teddyBearHolesParent = GameObject.Find ("Holes");
+			teddyBearHolesChildren = teddyBearHolesParent.transform.GetComponentsInChildren<Transform> ();		
+		}
+	}
+	
+	protected void Awake()
+	{
+		SetupLocal();
+	}
+	
+	protected void Start () 
+	{
+		SetupGlobal();
+	}
+	
+	protected void Update () 
+	{
+		//Checks vicinity of the player to the Teddybear
+		if (Vector3.Distance (this.transform.position, mainCharacter.transform.position) < 10f) {
+			mainCharacterIsClose = true;
+			
+		} 
+		else 
+		{
+			mainCharacterIsClose = false;
+		}
+
+		//Action when player is close
+		if (mainCharacterIsClose && !startRunningToHole) 
+		{
+			//Run to the nearest TeddybearHole object
+			foreach(Transform hole in teddyBearHolesChildren)
+			{
+				if( hole == teddyBearHolesParent.transform || hole == null)
+				{
+					continue;
+				}
+				if(closestHole == null || Vector3.Distance(this.transform.position, hole.transform.position) < Vector3.Distance(this.transform.position, closestHole.transform.position)) 
+				{
+					closestHole = hole;
+				} 
+			}
+			startRunningToHole = true;
+
+
+			//if none present or all are taken, just run away in the direction of the game
+			//RunAway ();	
+		}
+
+		if (startRunningToHole) 
+		{
+			RunToHole(closestHole);
+			startRunningToHole = false;
+		}
+	}
+
+	protected void RunToHole(Transform hole)
+	{
+		//Move to hole
+		Debug.Log("Running to hole wee wee " + hole.name);
+		//Vector3 relativePos = this.transform.position - hole.position;
+		//this.transform.Translate(relativePos.normalized * 25.0f * Time.deltaTime, Space.World);
+
+		this.gameObject.MoveTo (hole.position).Speed (25.0f).Execute ();
+	}
+
+	protected void RunAway() 
+	{
+		//Runs away from target by half of the mainCharacter's speed
+		Vector3 relativePos = this.transform.position - mainCharacter.transform.position;
+		transform.Translate (relativePos.normalized * 25.0f * Time.deltaTime);	
+	}
+
+}
