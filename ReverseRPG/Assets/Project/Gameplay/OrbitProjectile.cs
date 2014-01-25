e2b09a360cd082e9cbdf6f877099654c2b61d1ac
Robin Marx <robin@lugus-studios.be>
2014-01-25 12:21:28 +0100

    - Basic isometric setup with 2 camera's (damn you z-order shader problems!)

---
+++ ReverseRPG/Assets/Project/Gameplay/OrbitProjectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OrbitProjectile : MonoBehaviour 
+{
+	public void SetupLocal()
+	{
+		// assign variables that have to do with this class only
+	}
+	
+	public void SetupGlobal()
+	{
+		// lookup references to objects / scripts outside of this script
+	}
+	
+	protected void Awake()
+	{
+		SetupLocal();
+	}
+
+	protected void Start () 
+	{
+		SetupGlobal();
+	}
+	
+	protected void Update () 
+	{
+	
+	}
+}
