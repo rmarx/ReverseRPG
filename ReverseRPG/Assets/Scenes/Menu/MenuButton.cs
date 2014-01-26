98faa8657a9e933f2f411adb8f1dd52d53b10732
Robin Marx <robin@lugus-studios.be>
2014-01-26 13:27:48 +0100

    - Added Mainmenu

---
+++ ReverseRPG/Assets/Scenes/Menu/MenuButton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuButton : MonoBehaviour 
+{
+	public bool newGameButton = true;
+
+	public void SetupLocal()
+	{
+		// assign variables that have to do with this class only
+	}
+	
+	public void SetupGlobal()
+	{
+		// lookup references to objects / scripts outside of this script
+	}
+	
+	protected void Awake()
+	{
+		SetupLocal();
+	}
+
+	protected void Start () 
+	{
+		SetupGlobal();
+	}
+	
+	protected void Update () 
+	{
+		if( LugusInput.use.RayCastFromMouse(LugusCamera.ui) == this.transform )
+		{
+			this.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+
+			if( LugusInput.use.down )
+			{
+				if( newGameButton )
+				{
+					Application.LoadLevel("Game");
+				}
+				else
+				{
+					#if UNITY_STANDALONE_WIN
+					System.Diagnostics.Process.GetCurrentProcess().Kill();
+#else
+					Application.Quit();
+#endif
+
+				}
+			}
+		}
+		else
+		{
+			this.transform.localScale = Vector3.one;
+		}
+	}
+}
